4cae4ff43452c65fadff6d18b5f074df2dad2742
Robert Gordon <Komeuppance@gmail.com>
2020-01-21 17:45:29 -0800

    Added router

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountRouter = require('./accounts/accounts-router.js');
+
+// const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h3>Server online.</h3>');
+  });
+  
+  server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+      message: 'Something went wrong'
+    })
+  })
 
 module.exports = server;
